f93d43d8096f4467b023bf26c980b9f37c0da827
Chirack96 <jacques-chirack.gougou@epitech.eu>
2024-05-30 16:47:26 +0200

    feat: Fonction Emprunt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import classes.Livre;
+import classes.Category;
+import exception.LivreNonDisponibleException;
+
 public class Main {
     public static void main(String[] args) {
 
+        Livre[] catalogue = new Livre[10];
+        for (int i = 0; i < 10; i++) {
+            catalogue[i] = new Livre("Titre " + (i+1), "Autheur " + (i+1), "ID" + (i+1), Category.FICTION);
+        }
+
+        try {
+            catalogue[0].emprunter();
+            System.out.println("Le livre a bien été emprunté.");
+        } catch (LivreNonDisponibleException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/classes/Livre.java
+++ src/classes/Livre.java
@@ -1,8 +1,36 @@
 package classes;
+import exception.LivreNonDisponibleException;
+import methodes.Empruntable;
 
-public class Livre {
+public class Livre implements Empruntable {
     String titre;
     String auteur;
     String idbook;
-    boolean disponible;
+    public boolean disponible;
+    Category category;
+
+    public Livre(String titre, String auteur, String idbook, Category category) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.idbook = idbook;
+        this.disponible = true;
+        this.category = category;
+    }
+
+    public void emprunter() throws LivreNonDisponibleException {
+        if(!disponible){
+            throw new LivreNonDisponibleException("Le livre n'est pas disponible");
+        }
+        disponible = false;
+    }
+public void rendre() throws LivreNonDisponibleException {
+        if(disponible){
+            throw new LivreNonDisponibleException("Le livre est actuellement disponible");
+        }
+        disponible = true;
+    }
+    public String toString() {
+        return "Titre: " + titre + ", Auteur: " + auteur + ", ID: " + idbook + ", Disponible: " + disponible;
+    }
+
 }
