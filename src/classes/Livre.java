38851fdc76285779ef26df560527a9685015d190
bobcat88 <98208683+bobcat88@users.noreply.github.com>
2024-06-04 10:04:05 +0200

    classes

--- src/classes/Livre.java
+++ src/classes/Livre.java
@@ -1,8 +1,24 @@
-package classes;
-
 public class Livre {
-    String titre;
-    String auteur;
-    String idbook;
-    boolean disponible;
-}
+    private String titre;
+    private String auteur;
+    private String idbook;
+    private boolean disponible;
+
+    // Constructeur
+    public Livre(String titre, String auteur, String idbook, boolean disponible) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.idbook = idbook;
+        this.disponible = disponible;
+    }
+
+    // Getters and Setters
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String get
