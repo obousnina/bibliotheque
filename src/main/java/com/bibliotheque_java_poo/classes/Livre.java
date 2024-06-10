372b4d4f2d929b8991968938ffe9e02aa318e751
Constantin <istrateconstantin6@gmail.com>
2024-06-10 19:48:40 +0200

    Reading booksDatabase.json file and turn his content into an java list of Livre,done

--- src/main/java/com/bibliotheque_java_poo/classes/Livre.java
+++ src/main/java/com/bibliotheque_java_poo/classes/Livre.java
@@ -3,6 +3,7 @@ import com.bibliotheque_java_poo.exceptions.LivreNonDisponibleException;
 import com.bibliotheque_java_poo.interfaces.Empruntable;
 
 public class Livre implements Empruntable {
+
     private String titre;
     private String auteur;
     private String idBook;
@@ -21,6 +22,7 @@ public class Livre implements Empruntable {
         this.titre = titre;
         this.auteur = auteur;
         this.idBook = idBook;
+        this.categorie = categorie;
         this.disponible = false;
     }
 
@@ -38,6 +40,46 @@ public class Livre implements Empruntable {
     @Override
     public void rendre () {
         this.disponible = true;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public String getIdBook() {
+        return idBook;
+    }
+
+    public void setIdBook(String idBook) {
+        this.idBook = idBook;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
     }
 }
 
--- src/main/java/com/bibliotheque_java_poo/utils/booksMethods.java
+++ src/main/java/com/bibliotheque_java_poo/utils/booksMethods.java
@@ -1,31 +1,30 @@
 package com.bibliotheque_java_poo.utils;
 
-import com.bibliotheque_java_poo.classes.Livre;
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import javax.lang.model.type.ArrayType;
 
-import static com.bibliotheque_java_poo.classes.Livre.Categorie;
+import  com.bibliotheque_java_poo.classes.Livre;
 
 public class booksMethods {
     public static void getBooksList () {
         String userDir = System.getProperty("user.dir");
         File fileToReadFrom = new File(userDir +"/src/main/java/com/bibliotheque_java_poo/data/booksDatabase.json");
         Charset charsetOfTheFileToReadFrom = Charset.defaultCharset();
+        String booksSerialization= "";
+        String line = null;
 
         try(BufferedReader reader = Files.newBufferedReader(Paths.get(userDir + "/src/main/java/com/bibliotheque_java_poo/data/booksDatabase.json"), charsetOfTheFileToReadFrom);) {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                Livre[] booksList[0] = new Livre("","","",Categorie.FICTION);
-                booksList.add(line);
-            }
 
-            System.out.println(booksList);
+            while ((line = reader.readLine()) != null) {
+                booksSerialization +=line;
+            }
+            Gson gson = new Gson();
+            Livre[] booksList = gson.fromJson(booksSerialization, Livre[].class);
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
